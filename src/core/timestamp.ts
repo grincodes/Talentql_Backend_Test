2bd5f98204b789c514da260217388f4fffa8aba8
designerseyi <designerseyi@gmail.com>
2022-09-12 08:42:46 +0100

    timestamp validation

--- src/core/timestamp.ts
+++ src/core/timestamp.ts
@@ -1,36 +1,33 @@
+import moment from "moment";
 import { Result } from "../utils/Result";
 
 export interface TimestampProps {
-    timestamp: number
+  timestamp: number;
 }
 
 export class Timestamp {
+  protected props;
 
-    protected props;
-
-    get value () : number {
+  get value(): number {
     return this.props.timestamp;
+  }
+  private constructor(props: TimestampProps) {
+    this.props = props;
+  }
+
+  public static isValidTimeStamp(timestamp: number): boolean {
+    //  const valid = (new Date(timestamp)).getTime() > 0;
+    const valid = moment(timestamp).isValid();
+    return valid;
+  }
+
+  public static create(props: TimestampProps): Result<Timestamp> {
+    if (!this.isValidTimeStamp(props.timestamp)) {
+      return Result.fail<Timestamp>("Invalid TimeStamp");
+    } else {
+      return Result.ok<Timestamp>(
+        new Timestamp({ timestamp: props.timestamp })
+      );
     }
-    private constructor(props:TimestampProps) {
-        this.props = props
-    }
-
-    public static isValidTimeStamp(timestamp:number):boolean{
-         const valid = (new Date(timestamp)).getTime() > 0;
-         return valid
-    }
-
-    public static create(props:TimestampProps):Result<Timestamp>{
-
-        if (!this.isValidTimeStamp(props.timestamp)) {
-        return Result.fail<Timestamp>('Invalid TimeStamp');
-        
-        } else {
-        return Result.ok<Timestamp>(
-            new Timestamp({ timestamp: props.timestamp })
-        );
-        }
-
-    }
-
+  }
 }
