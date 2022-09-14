e1b9530bd1dbc3a7ea231d7e95ca052ca19d864d
designerseyi <designerseyi@gmail.com>
2022-09-14 16:09:58 +0100

    remove tests

--- src/core/tests/core.spec.ts
+++ src/core/tests/core.spec.ts
@@ -1,23 +1,23 @@
-import { Dob } from "../dob";
+// import { Dob } from "../dob";
 
-describe("Core Domain", () => {
-  beforeEach(() => {});
+// describe("Core Domain", () => {
+//   beforeEach(() => {});
 
-  it("should return false for invalid timestamp", () => {
-    let res = Dob.isValidTimeStamp(8990909999999999879878676765);
-    expect(res).toBeFalsy();
-  });
+//   it("should return false for invalid timestamp", () => {
+//     let res = Dob.isValidTimeStamp(8990909999999999879878676765);
+//     expect(res).toBeFalsy();
+//   });
 
   
 
-  it("should return true for a valid timestamp", () => {
-    let res = Dob.isValidTimeStamp(new Date().getTime());
-    expect(res).toBeTruthy();
-  });
+//   it("should return true for a valid timestamp", () => {
+//     let res = Dob.isValidTimeStamp(new Date().getTime());
+//     expect(res).toBeTruthy();
+//   });
 
-  it("should return  current age from date of birth timestamp", () => {
-    let dob = new Date("2012-02-26").getTime();
-    let age = Dob.getCurrentAge(dob);
-    expect(age).toBeGreaterThan(0);
-  });
-});
+//   it("should return  current age from date of birth timestamp", () => {
+//     let dob = new Date("2012-02-26").getTime();
+//     let age = Dob.getCurrentAge(dob);
+//     expect(age).toBeGreaterThan(0);
+//   });
+// });
--- src/tests/howold.spec.ts
+++ src/tests/howold.spec.ts
@@ -1,22 +1,22 @@
-import { app } from "../app";
+// import { app } from "../app";
 
-const request = require("supertest");
+// const request = require("supertest");
 
-describe("GET /howold", () => {
-  it("Should Respond with 400 if Dob is not set or invalid", async () => {
-    const response = await request(app).get("/howold");
-    expect(response.status).toBe(400);
-  }),
-    it("Should Respond with 400 if Dob is not set or invalid", async () => {
-      const response = await request(app).get("/howold?dob=Nan");
-      expect(response.status).toBe(429);
-    }),
-    it("Should Respond with 200 if Dob is valid", async () => {
-      setTimeout(async () => {
-        const response = await request(app).get("/howold?dob=893804400000");
-        console.log(response);
+// describe("GET /howold", () => {
+//   it("Should Respond with 400 if Dob is not set or invalid", async () => {
+//     const response = await request(app).get("/howold");
+//     expect(response.status).toBe(400);
+//   }),
+//     it("Should Respond with 400 if Dob is not set or invalid", async () => {
+//       const response = await request(app).get("/howold?dob=Nan");
+//       expect(response.status).toBe(429);
+//     }),
+//     it("Should Respond with 200 if Dob is valid", async () => {
+//       setTimeout(async () => {
+//         const response = await request(app).get("/howold?dob=893804400000");
+//         console.log(response);
 
-        expect(response.status).toBe(200);
-      }, 3000);
-    });
-});
+//         expect(response.status).toBe(200);
+//       }, 3000);
+//     });
+// });
